f50d986eb981372940fc873eee2cf2a84a68a141
Andrew Jensen <andrew.jensen@domo.com>
2016-02-25 14:34:07 -0700

    feat(): Clean up entry point in webpack.config.js by adding a simple API.

--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,17 @@
+// Include dependencies
 require('babel-register');
-module.exports = require('./other/webpack.config.es6');
+const getConfig = require('./other/webpack.config.es6');
+
+/**
+ * Configure your webpack setup here. These settings can be changed at any time.
+ */
+module.exports = getConfig({
+  includeDesktopView: true,
+  includeMobileView: true,
+  externals: [
+    // Include your app's extra externals here
+  ],
+  loaders: [
+    // Include your app's extra loaders here
+  ]
+});
