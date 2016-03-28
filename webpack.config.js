9db4a5a709cadd517a124958e4916239645e5afe
Andrew Jensen <andrew.jensen@domo.com>
2016-03-28 10:38:04 -0600

    doc(): Add link to wiki in webpack.config.js.

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,9 @@ const getConfig = require('./other/webpack.config.es6');
 
 /**
  * Configure your webpack setup here. These settings can be changed at any time.
+ *
+ * Read more on the wiki:
+ * https://git.empdev.domo.com/AppTeam6/da-webpack/wiki/Webpack-Configuration
  */
 module.exports = getConfig({
   includeDesktopView: true,
