a499c7aade949caf00e7f57151d8925c7b9a2cc7
Christopher <chris.morgan@domo.com>
2016-03-28 12:49:24 -0600

    Update update.js
    Mv open script code into the update function

--- other/update.js
+++ other/update.js
@@ -4,20 +4,29 @@ const shelljs = require('shelljs');
 const path = require('path');
 const chalk = require('chalk');
 
-if (hasUncommittedChanges()) {
-  console.log(`${chalk.red('ERROR:')} you have uncommitted changes. Please commit or stash them before updating.`);
-  process.exit(1);
-}
-
-if (!hasGeneratorRemote()) {
-  console.log(`${chalk.red('ERROR:')} no ${chalk.bold('generator')} remote found. Exiting.`);
-  process.exit(1);
-}
-
 update();
 
-console.log(`${chalk.yellow('WARNING:')} Make sure to review the changes with ${chalk.bold('git diff HEAD')} before ` +
+
+#//////////////////////////////////////////////
+
+
+function update() {
+  if (hasUncommittedChanges()) {
+    console.log(`${chalk.red('ERROR:')} you have uncommitted changes. Please commit or stash them before updating.`);
+    process.exit(1);
+  }
+
+  if (!hasGeneratorRemote()) {
+    console.log(`${chalk.red('ERROR:')} no ${chalk.bold('generator')} remote found. Exiting.`);
+    process.exit(1);
+  }
+
+  shelljs.exec('git fetch generator');
+  shelljs.exec('git merge --no-commit generator/master');
+  
+  console.log(`${chalk.yellow('WARNING:')} Make sure to review the changes with ${chalk.bold('git diff HEAD')} before ` +
             `committing, so no accidental changes are made to your app.`);
+}
 
 function hasUncommittedChanges() {
   const statusOutput = shelljs.exec('git status --porcelain', { silent: true }).output;
@@ -35,9 +44,4 @@ function hasGeneratorRemote() {
   });
 
   return found;
-}
-
-function update() {
-  shelljs.exec('git fetch generator');
-  shelljs.exec('git merge --no-commit generator/master');
 }
