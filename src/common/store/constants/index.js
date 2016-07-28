2b6ac43f23d5d45984109ce9b67e01bf3faec245
sldavidson <scott.davidson9@gmail.com>
2016-07-28 12:04:37 -0600

    Update index.js

--- src/common/store/constants/index.js
+++ src/common/store/constants/index.js
@@ -1,9 +1,9 @@
 const constants = generateConstants([
 /*
   Action constants go here. Constants should uppper snake cased.
-  They should have the pattern of <NOUN>_<VERB PAST TENSE>
   'DATA_REQUESTED',
-  'BUTTON_CLICKED'
+  'BUTTON_CLICKED',
+  'INCREASE_COUNT'
  */
 ]);
 
