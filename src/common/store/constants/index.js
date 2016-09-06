01035943168c8dd01cfe3bd5854ad2c6571e6ac6
Scott-Davidson <scott.davidson@domo.com>
2016-09-06 13:35:21 -0600

    fix(): corrected the constants examples in constants file. DAS-4096

--- src/common/store/constants/index.js
+++ src/common/store/constants/index.js
@@ -1,8 +1,8 @@
 /*
  * Export named constants only
  * e.g.
- * export const MY_ACTION_CONST = MY_ACTION_CONST;
- * export const MY_ACTION_2_CONST = MY_ACTION_2_CONST;
+ * export const MY_ACTION_CONST = 'MY_ACTION_CONST';
+ * export const MY_ACTION_2_CONST = 'MY_ACTION_2_CONST';
  *
  * This allows for proper ES6 module importing into files
  * e.g.
